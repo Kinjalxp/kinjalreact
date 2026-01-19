242a8faba3faa49702529580f033c0d77a423b05
Kinjal Maurya <kinjalxp@gmail.com>
2026-01-19 12:56:41 -0500

    Header customized

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
+import { User, BookOpen, Icon } from "lucide-react";
+
+const navItemClass = ({ isActive }: { isActive: boolean }) =>
+  ` flex items-center gap-2
+   px-8 py-3
+   rounded-md transition-colors
+  ${
+    isActive
+      ? "bg-black text-white px-4 py-2 rounded"
+      : "bg-white text-black px-4 py-2 rounded border"
+  }`;
+
+const navItems = [
+  { label: "About", path: "/", icon: User, end: true },
+  { label: "Blog", path: "/blog", icon: BookOpen },
+];
 
 const App = () => {
   return (
     <BrowserRouter>
-      <nav className="flex gap-6 p-4 border-b">
-        <Link to="/" className="hover:text-blue-500">
-          About
-        </Link>
-        <Link to="/blog" className="hover:text-blue-500">
-          Blog
-        </Link>
+      <nav className="flex gap-4 p-4 border-b">
+        <div className="ml-auto flex gap-4">
+          {navItems.map(({ label, path, icon: Icon, end }) => (
+            <NavLink key={path} to={path} end={end} className={navItemClass}>
+              <Icon size={18} />
+              <span>{label}</span>
+            </NavLink>
+          ))}
+        </div>
       </nav>
 
       <Routes>
